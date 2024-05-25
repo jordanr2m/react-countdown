aa24f5f4f615690da67ae20c0d98257901095923
Jordan J <jordanr2m@gmail.com>
2024-05-25 11:35:42 -0700

    Added wedding dress image.

--- src/components/product/Product.js
+++ src/components/product/Product.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "./Product.css"
 import { AiOutlineShoppingCart } from "react-icons/ai";
+import gown from "../../assets/gown1.jpg"
 
 const Product = () => {
   return (
@@ -11,13 +12,21 @@ const Product = () => {
           {/* Place icon inside of h2 */}
           <h2 className='--text-md --text-light --py2'>
             Zulu<span>Shop</span>
-            <AiOutlineShoppingCart color="orangered" size={30} className='shopping-icon'/>
+            <AiOutlineShoppingCart color="orangered" size={30} className='shopping-icon' />
           </h2>
         </nav>
       </header>
 
       <section>
-        
+        <div className='container product --flex-center --flex-dr-column'>
+          <div className='product-image --text-center --card --mr'>
+            <img src={gown} alt='wedding dress'/>
+          </div>
+
+          <div className='product-description'>
+
+          </div>
+        </div>
       </section>
     </>
   )
